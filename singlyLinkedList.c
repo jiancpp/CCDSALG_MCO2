22edb02acf46e67af0715f358495b37ac85a00b4
eepy-elo <shieloheart.lunario@gmail.com>
2025-07-08 20:19:29 +0800

    Implemented  Singly Linked Lists

---
+++ singlyLinkedList.c
@@ -0,0 +1,67 @@
+// Include other standard libraries here
+#include <stdlib.h>
+
+#include "singlyLinkedList.h"
+
+void freeList(List list) {
+    list = NULL;
+}
+
+Node* createNode(Vertex data) {
+    Node* pNew;
+    pNew = (Node*) malloc (sizeof(Node));
+    pNew->data = data;
+    pNew->pNext = NULL; 
+
+    return pNew;
+}
+
+void insertAtEnd(List* list, Node* pNew) {
+    Node* pTemp = *list;
+    pNew->pNext = NULL;
+
+    if (*list == NULL) {
+        *list = pNew;
+        return;
+    }
+        
+    while (pTemp->pNext != NULL)
+        pTemp = pTemp->pNext;
+    
+    pTemp->pNext = pNew;
+}
+
+void deleteFromEnd(List* list) {
+    Node* pTemp = *list;
+
+    if ((*list) == NULL) 
+        return;
+    
+    if (pTemp->pNext == NULL) {
+        freeNode(pTemp);
+        *list = NULL;
+        return;
+    }
+
+    while (pTemp->pNext->pNext != NULL)
+        pTemp = pTemp->pNext;
+
+    freeNode(pTemp->pNext);
+    pTemp->pNext == NULL;
+}
+
+Node* search(List list, Vertex data) {
+    Node* pCur = list;
+
+    while (pCur != NULL) {
+        if (strcmp(pCur->data.name, data.name) == 0)
+            return pCur;
+        pCur = pCur->pNext;    
+    }
+
+    return NULL;
+}
+
+void freeNode(Node* node) {
+    node = NULL;
+}
---
+++ singlyLinkedList.h
@@ -0,0 +1,20 @@
+#ifndef SINGLYLINKEDLIST_H
+#define SINGLYLINKEDLIST_H
+
+#include "graph.h"
+
+struct singlyNodeTag {
+    Vertex data;
+    Node* pNext;
+};
+
+typedef struct singlyNodeTag Node;
+typedef struct singlyNodeTag* List;
+
+void freeList(List list);
+void insertAtEnd(List* list, Node* pNew);
+void deleteFromEnd(List* list);
+Node* search(List list, Vertex data);
+void freeNode(Node* node);
+
+#endif

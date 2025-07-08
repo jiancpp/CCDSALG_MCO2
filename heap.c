e1cdb6d2dbfafde8966de42a77a140ff9951ae7c
jiancpp <117244684+jiancpp@users.noreply.github.com>
2025-07-08 15:55:14 +0800

    Try heap implementation

--- heap.c
+++ heap.c
@@ -1,3 +1,45 @@
 // Include other standard libraries here
 
 #include "heap.h"
+
+/**
+ * This function initializes a heap by setting the last (last index) to -1.
+ * @param heap the heap to be initialized
+ */
+void initializeHeap(Heap* heap) 
+{
+    heap->capacity = MAX_CAPACITY;
+    heap->last = -1;
+}
+
+/**
+ * This function swaps two elements in the heap
+ * @param a an element within a heap
+ * @param b an element within a heap
+ */
+void swap(int* a, int* b) 
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Insert
+
+// Extract Min/Max
+
+// Peek
+
+
+// Delete
+
+// Increase/Decrease Key
+
+// Build Heap
+
+
+/*
+References:
+https://www.tutorialspoint.com/data_structures_algorithms/heap_data_structure.htm
+
+*/
--- heap.h
+++ heap.h
@@ -1,6 +1,12 @@
 #ifndef HEAP_H
 #define HEAP_H
 
+#include "shared.h"
+typedef struct {
+    int array[MAX_CAPACITY];
+    int capacity;
+    int last;
+} Heap;
 
 #endif
 

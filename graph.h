1c164907f547390117e9d0d0fdb45debe75b19a9
jiancpp <117244684+jiancpp@users.noreply.github.com>
2025-07-08 22:12:02 +0800

    Reset Graph

--- graph.h
+++ graph.h
@@ -3,30 +3,6 @@
 
 #include "shared.h"
 
-#define MAX_VERTICES 128
-#define MAX_EDGES 256
-struct VertexTag{
-    String256 name;
-    int degree;
-};
 
-struct EdgeTag{
-    Vertex* initial;
-    Vertex* terminal;
-    int weight;
-};
-
-struct GraphTag
-{
-    String256 name;
-    Vertex vertices[MAX_VERTICES];
-    Edge edges[MAX_EDGES];
-    int verticesSize;
-    int edgesSize;
-};
-
-typedef struct VertexTag Vertex;
-typedef struct EdgeTag Edge;
-typedef struct GraphTag Graph;
 
 #endif

8014e22f3f108e6832d8a3397c6813cc4a9cec4c
jiancpp <117244684+jiancpp@users.noreply.github.com>
2025-07-15 20:33:50 +0800

    Linked List for Graph

--- graph.h
+++ graph.h
@@ -3,10 +3,10 @@
 
 #include "shared.h"
 #include "linkedListGraph.h"
-
 struct Graph {
     int numVertices;
-    VertexList vertexListHead; // Dapat ba may pointer?
+    Vertex* vertexList; // Dynamic Array of vertices
+    Edge* edgeList; // Dynamic Array of Edge structs
 };
 
 typedef struct Graph Graph;
@@ -14,7 +14,7 @@ typedef struct Graph Graph;
 // Basic Graph Instantiation
 Graph* createGraph();
 void addVertex(Graph* graph, String256 vertex);
-void addEdge(Graph* graph, String256 src, String256 dest);
+void addEdge(Graph* graph, String256 vertex1, String256 vertex2);
 int getDegree(Graph* graph, String256 vertex);
 bool edgeExists(Graph* graph, String256 src, String256 dest);
 void printGraph(Graph* graph);
--- linkedListGraph.h
+++ linkedListGraph.h
@@ -1,37 +1,35 @@
-#ifndef GRAPHLINKEDLIST_H
+#ifndef LINKEDLISTGRAPH_H
 
-#define GRAPHLINKEDLIST_H
+#define LINKEDLISTGRAPH_H
 
 #include "shared.h"
 
+struct Vertex {
+    String256 vertex;
+    EdgeList edgeListHead; // Linked list of EdgeNode structs
+};
+
+struct Edge {
+    Vertex* vertex_a;
+    Vertex* vertex_b;
+    int weight;
+};
+
 struct EdgeNode {
-    VertexNode* dest;
+    Vertex* adjVertex;
     int weight;
     EdgeNode* next;
 };
 
-struct VertexNode {
-    String256 vertex;
-    EdgeList edgeListHead;
-    VertexNode* next;
-};
-
+typedef struct Vertex Vertex;
+typedef struct VertexNode VertexNode;
+typedef struct Edge Edge;
 typedef struct EdgeNode EdgeNode;
 typedef struct EdgeNode* EdgeList;
-typedef struct VertexNode VertexNode;
-typedef struct VertexNode* VertexList;
-
-// Vertex Linked List Operations
-void freeVertexList(VertexList* listHead);
-VertexNode* createVertexNode(String256 vertex);
-void insertVertexAtEnd(VertexList* listHead, VertexNode* pNew);
-void deleteVertexFromEnd(VertexList* listHead);
-VertexNode* searchVertex(VertexList listHead, String256 key);
-void freeVertexNode(VertexNode* node);
 
 // Edge Linked List Operations
 void freeEdgeList(EdgeList* listHead);
-EdgeNode* createEdgeNode(VertexNode* dest, int weight);
+EdgeNode* createEdgeNode(VertexNode* adjVertex, int weight);
 void insertEdgeAtEnd(EdgeList* listHead, EdgeNode* pNew);
 void deleteEdgeFromEnd(EdgeList* list);
 EdgeNode* searchEdge(EdgeList listHead, String256 key);

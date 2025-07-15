df735257f45abe27bd60555d281dee100c161ed2
jiancpp <117244684+jiancpp@users.noreply.github.com>
2025-07-15 20:25:33 +0800

    Notes on Prim's Algo

--- traversal.c
+++ traversal.c
@@ -1,3 +1,14 @@
 // Include other standard libraries here
 
 #include "traversal.h"
+
+/*
+Getting MST
+- Connecting everything with everything else with Minimum weight
+
+Sort edges by weight before doing prim's algo
+
+while no. of edges is less than n minus one
+
+union find disjoint set for crosscalsssssss cycle checking
+*/

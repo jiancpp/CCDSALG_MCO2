cee747d38a014f13f73c3bc0c892c3c78f5a34d0
eepy-elo <shieloheart.lunario@gmail.com>
2025-07-22 04:46:23 +0800

    Edited printMST

--- MST.c
+++ MST.c
@@ -26,7 +26,25 @@ totalEdgeWeight(Graph* graph)
 void
 printMST(Graph* graph) 
 {
-    printf("MST");
-    printGraph(graph);
+    printf("MST(G) = (V, E)\n");
+
+    printf("V = {");
+    for (int i = 0; i < graph->numVertices; i++){
+        printf("%s", graph->vertexList[i].vertex);
+
+        if (i != graph->numVertices - 1)
+            printf(", ");
+        else
+            printf("}\n");
+    }
+
+    printf("E = {\n");
+    for (int i = 0; i < graph->numEdges; i++){
+        Edge temp = graph->edgeList[i];
+        printf("     (%s, %s, %d)\n", temp.vertex_a->vertex,
+                                      temp.vertex_b->vertex,
+                                      temp.weight);
+    }
+    printf("}\n");
     printf("Total Edge Weight: %d\n", totalEdgeWeight(graph));
 }

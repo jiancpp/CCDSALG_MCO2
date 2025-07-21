55a84628bb04d39b33b52b5eaa6c943062712b45
eepy-elo <shieloheart.lunario@gmail.com>
2025-07-22 03:38:03 +0800

    Drafted main

--- main.c
+++ main.c
@@ -1,7 +1,95 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 #include "main.h"
 
-int main() {
-    
+void
+tokenizeInput(String256 input, String256 tokens[])
+{
+    int i = 0;
+    char* token;
+
+    token = strtok(input, " \n");
+
+    while (token != NULL && i < MAX_TOKENS) {
+        strcpy(tokens[i++], token);
+        token = strtok(NULL, " \n");
+    }
+}
+
+void
+clearTokens(String256 tokens[]) 
+{
+    int i;
+    for (i = 0; i < MAX_TOKENS; i++)
+        strcpy(tokens[i], "\0");
+}
+
+int main() 
+{
+    String256 input;
+    String256 tokens[MAX_TOKENS];
+    Queue traversal;
+    Graph graph = createGraph();
+    // Graph graphMST = createGraph();
+
+    while (fgets(input, sizeof(input), stdin) != NULL 
+           && strcmp(input, "11\n") != 0) {
+        
+        // Initialize
+        clearQueue(&traversal);
+        clearTokens(tokens);
+
+        tokenizeInput(input, tokens);
+
+        switch (atoi(tokens[0])) {
+            case 1:
+                addVertex(&graph, tokens[1]);
+                break;
+            case 2:
+                addEdge(&graph, tokens[1], tokens[2], atoi(tokens[3]));
+                break;
+            case 3:
+                printf("%d\n", getDegree(graph, tokens[1]));
+                break;
+            case 4:
+                printf("%d\n", edgeExists(&graph, tokens[1], tokens[2]));
+                break;
+            case 5:
+                traversal = BFS(&graph, tokens[1]);
+                while (!isEmptyQueue(&traversal)) {
+                    printf("%s\n", dequeue(&traversal).vertex);
+                }
+                break;
+            case 6:
+                // traversal = DFS(&graph, tokens[1]);
+                // while (!isEmptyQueue(&traversal)) {
+                //     printf("%s\n", dequeue(&traversal).vertex);
+                // }
+                break;
+            case 7:
+                printf("%d\n", checkPath(&graph, tokens[1], tokens[2]));
+                break;
+            case 8:
+                // MST
+                // generateMST(&graph, &graphMST);
+                // printGraph(&graphMST);
+                // printf("Total Edge Weight: %d\n", totalEdgeWeight(&graphMST));
+                break;
+            case 9:
+                // Shortest Path
+                // printShortestPath(&graph);
+                // printf("Total edge cost: %d\n", totalEdgeCost(path));
+                break;
+            case 10:
+                printGraph(&graph);
+                break;
+            default:
+                break;
+        }
+    }
+
+    return 0;
 }
--- main.h
+++ main.h
@@ -1,12 +1,15 @@
 #ifndef MAIN_H
 
 #define MAIN_H
+#define MAX_TOKENS 4
 
+#include "shared.h"
 #include "stack.h"
 #include "queue.h"
 #include "heap.h"
 #include "graph.h"
 #include "traversal.h"
+#include "linkedListGraph.h"
 
 #endif
 

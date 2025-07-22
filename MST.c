a8c3cb4b95a7b1ee0cdcb495a5f5995bbd756eb5
jesmaeca <jezzisbored@gmail.com>
2025-07-22 23:22:06 +0800

    Added createMST

--- MST.c
+++ MST.c
@@ -9,16 +9,73 @@ Graph
 createMST(Graph* graph)
 {
     Graph graphMST = createGraph();
-    // Code here
+    Heap heap;
+    int i, idxA, idxB, vertexIdx = 0;
+    Edge tempEdge, minEdge;
+    EdgeNode *curEdgeNode;
+    Vertex *curVertex;
+    bool isVisited[graph->numVertices];
 
+    // Initializes
+    for (i = 0; i < graph->numVertices; i++)
+        isVisited[i] = false;
+
+    initializeHeap(&heap);
+
+    vertexIdx = 0;
+
+    curVertex = &graph->vertexList[vertexIdx];
+
+
+    addVertex(&graphMST, curVertex->vertex);
+    isVisited[vertexIdx] = true;
+
+    while (graphMST.numVertices < graph->numVertices) {
+        curEdgeNode = curVertex->edgeListHead;
+
+        while (curEdgeNode != NULL) {
+            if (!isVisited[getVertexIdx(graph, curEdgeNode->adjVertex->vertex)]) {
+                tempEdge = createEdge(curVertex, curEdgeNode->adjVertex, curEdgeNode->weight);
+                HeapInsert(&heap, tempEdge);
+            }
+        curEdgeNode = curEdgeNode->next;
+        }
+        
+        do {
+            if (heap.heapSize == 0)
+                return graphMST;
+
+            minEdge = HeapExtractMinimum(&heap);
+
+            idxA = getVertexIdx(graph, minEdge.vertex_a->vertex);
+            idxB = getVertexIdx(graph, minEdge.vertex_b->vertex);
+
+        } while (isVisited[idxA] && isVisited[idxB]);
+
+        if (!isVisited[idxA]) {
+            addVertex(&graphMST, minEdge.vertex_a->vertex);
+            curVertex = &graph->vertexList[idxA];
+            isVisited[idxA] = true;
+
+        } else {
+            addVertex(&graphMST, minEdge.vertex_b->vertex);
+            curVertex = &graph->vertexList[idxB];
+            isVisited[idxB] = true;
+        }
+
+        graphMST.edgeList[graphMST.numEdges] = createEdge(minEdge.vertex_a, minEdge.vertex_b, minEdge.weight);
+        graphMST.numEdges++;
+    }
     return graphMST;
 }
 
 int
 totalEdgeWeight(Graph* graph)
 {
-    int sum = 0;
-    // Code here
+    int i, sum = 0;
+    for (i = 0; i < graph->numEdges; i++) {
+        sum += graph->edgeList[i].weight;
+    }
 
     return sum;
 } 
@@ -26,7 +83,7 @@ totalEdgeWeight(Graph* graph)
 void
 printMST(Graph* graph) 
 {
-    printf("MST(G) = (V, E)\n");
+    printf("MST(G) = (V, E).\n");
 
     printf("V = {");
     for (int i = 0; i < graph->numVertices; i++){
